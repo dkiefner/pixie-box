a968512bb293f3eed2ed1b4d60c63aa0ef4b24ea
Danny Kiefner <dannyk@spotify.com>
2022-06-06 20:59:33 +0200

    fix mkdir

--- scripts/new_tag.py
+++ scripts/new_tag.py
@@ -22,8 +22,8 @@ try:
         print(f"{rfidDir} already exists. Please delete directory manually and retry!")
     else:
         print(f"Creating dir {rfidDir}")
-        newDir.mkdir(755)
-        
+        os.mkdir(rfidDir)
+
         print(f"Moving files from {uploadDir} to {rfidDir}")
         allfiles = os.listdir(uploadDir)
         for f in allfiles:
