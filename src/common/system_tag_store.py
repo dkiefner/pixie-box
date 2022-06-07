4c9b53c4d500247f51c4d40b4d946025ac73c5e1
Danny Kiefner <dannyk@spotify.com>
2022-06-07 20:08:58 +0200

    add script to create system tags

---
+++ src/common/system_tag_store.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import dbm
+
+from src.common.logger import Logger
+
+
+class SystemTagStore:
+    __store_name = "system-tag-store"
+
+    def save(self, key, value):
+        Logger.log(f"Storing {key}={value}")
+        with dbm.open(self.__store_name, 'c') as store:
+            store[str(key)] = value
---
+++ src/new_system_tag.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from src.common.rfid_reader import MFRC522Reader
+from src.common.system_tag_store import SystemTagStore
+
+print("Which system action do you want to record?:")
+print("1) Stop playing music")
+print("\nUse 'q' to exit this script.")
+
+keyToCommandDict = {
+    "1": "mpc -q stop"
+}
+
+while True:
+    input_key = input()
+
+    if "q" == input_key.rstrip():
+        break
+
+    if input_key in keyToCommandDict:
+        print(f"Please use the RFID tag you want to link to the action '{keyToCommandDict[input_key]}'")
+        rfidReader = MFRC522Reader()
+        rfid_tag = rfidReader.read()
+        store = SystemTagStore()
+        store.save(rfid_tag, keyToCommandDict[input_key])
+        break
+    else:
+        print(f"Sorry, {input_key} is not a valid option.")
+
+print("Done.")
