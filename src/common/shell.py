9c354648c8d0d13433f389e449410e23bb0fe096
Danny Kiefner <dannyk@spotify.com>
2022-06-08 21:51:06 +0200

    add more logging for shell executions and pixiebox

--- src/common/shell.py
+++ src/common/shell.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 
+from common.logger import Logger
+
 
 class Shell:
     @staticmethod
     def execute(cmd):
+        Logger.log(f"Executing command: {cmd}")
         os.system(cmd)
--- src/pixiebox.py
+++ src/pixiebox.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from common.file_system import FileSystem
+from common.logger import Logger
 from common.player import LocalFilePlayer, SystemAudioUris
 from common.rfid_reader import MFRC522Reader
-from common.system_tag_store import SystemTagStore
 from common.shell import Shell
+from common.system_tag_store import SystemTagStore
 
 rfidReader = MFRC522Reader()
 player = LocalFilePlayer()
@@ -14,10 +15,15 @@ tag_id = rfidReader.read()
 
 tagDir = FileSystem.path(FileSystem.RFID_BASE_DIR, tag_id)
 if tagDir.exists():
+    Logger.log(f"Audio tag id scanned.")
     player.play_rfid(tag_id)
-elif systemTagStore.get(tag_id) is not None:
-    Shell.execute(systemTagStore.get(tag_id))
 else:
-    player.play_file(SystemAudioUris.SAD_TROMBONE)
+    cmd = systemTagStore.get(tag_id)
+    if cmd is not None:
+        Logger.log(f"System tag id scanned.")
+        Shell.execute(cmd)
+    else:
+        Logger.log(f"Tag id not registered.")
+        player.play_file(SystemAudioUris.SAD_TROMBONE)
 
 rfidReader.cleanup()
