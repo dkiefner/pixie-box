c70d4c92bc4edb03cacb957b2c400b6442cdaa7c
Danny Kiefner <dannyk@spotify.com>
2022-06-08 21:56:51 +0200

    add absolute path for system tag db

--- src/common/system_tag_store.py
+++ src/common/system_tag_store.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
 import dbm
+import os
 
 from common.logger import Logger
 
 
 class SystemTagStore:
-    __store_name = "system-tag-store"
+    __store_path = os.path.join("/home/pi/pixiebox", "system-tag-store")
 
     def save(self, key, value):
         Logger.log(f"Storing {key}={value}")
-        with dbm.open(self.__store_name, 'c') as store:
+        with dbm.open(self.__store_path, 'c') as store:
             store[str(key)] = value
             store.close()
 
     def get(self, key):
-        with dbm.open(self.__store_name, 'c') as store:
+        with dbm.open(self.__store_path, 'r') as store:
             value = store.get(str(key))
             store.close()
             return value
