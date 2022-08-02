7a068c4b5c3ff752da6d678a5c85b6a40ea83aa2
Danny Kiefner <dannyk@spotify.com>
2022-08-02 22:50:38 +0200

    remove timestamp from logger

--- backend/pixiebox/common/logger.py
+++ backend/pixiebox/common/logger.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-from datetime import datetime
-
-
 class Logger:
     @staticmethod
     def log(value):
-        print(f"{datetime.now()}: {value}")
+        print(f"{value}")
