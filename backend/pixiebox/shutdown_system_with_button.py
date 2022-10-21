2ba284834ebb080c0409befafc785422e5c14502
Danny Kiefner <dannyk@spotify.com>
2022-10-21 20:50:40 +0200

    simplify shutdown script and disable GPIO warnings

--- backend/pixiebox/shutdown_system_with_button.py
+++ backend/pixiebox/shutdown_system_with_button.py
@@ -5,19 +5,13 @@ import RPi.GPIO as GPIO
 from common.logger import Logger
 from common.shell import Shell
 
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+Logger.log("Waiting for button press...")
+GPIO.wait_for_edge(3, GPIO.FALLING)
 
-def listen_for_button_press(on_button_pressed):
-    Logger.log("Waiting for button press...")
-    GPIO.wait_for_edge(3, GPIO.FALLING)
-    on_button_pressed()
-
-
-def shutdown_system():
-    Logger.log("Shutting down system.")
-    Shell.execute("shutdown -h now")
-
-
-listen_for_button_press(shutdown_system)
+Logger.log("Shutting down system.")
+Shell.execute("shutdown -h now")
