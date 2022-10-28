0cb7bfd693fbb1c2533d0f9ee7a2ea9473377fe9
Danny Kiefner <dannyk@spotify.com>
2022-10-28 20:49:39 +0200

    restore backup

--- app/lib/file_system.py
+++ app/lib/file_system.py
@@ -4,14 +4,15 @@ import os
 import pathlib
 
 from lib.logger import Logger
+from lib.shell import Shell
 
 
 class FileSystem:
     ROOT_DIR = "/home/pi/pixiebox/"
     DATA_DIR = f"{ROOT_DIR}data/"
     RFID_BASE_DIR = f"{DATA_DIR}audio/rfid/"
-    UPLOAD_DIR = f"{DATA_DIR}audio/upload/"
     TEMP_DIR = f"{ROOT_DIR}tmp/"
+    UPLOAD_DIR = f"{TEMP_DIR}upload/"
 
     @staticmethod
     def path(base_path, directory):
@@ -29,3 +30,15 @@ class FileSystem:
         for file in allfiles:
             Logger.log(f"Moving file: {str(file)}")
             os.rename(source + file, target + file)
+
+    @staticmethod
+    def save(file, path):
+        file_path = os.path.join(path, file.filename)
+        Logger.log(f"Saving file {file.filename} to {path}")
+        file.save(file_path)
+        return file_path
+
+    @staticmethod
+    def delete_content(directory_path):
+        Logger.log(f"Deleting all content in: {directory_path}")
+        Shell.execute(f"rm -rf {directory_path}*")
--- app/web/app.py
+++ app/web/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
 
 from lib.file_system import FileSystem
 from lib.shutdown import Shutdown
 from lib.zip import Zip
 
 app = Flask(__name__)
+app.config["UPLOAD_DIR"] = FileSystem.UPLOAD_DIR
 
 
 # css style from: https://moderncss.dev/custom-select-styles-with-pure-css/
@@ -13,9 +14,16 @@ def index():
     return render_template('index.html', rfids=["one", "two"])
 
 
-@app.route('/backup')
+@app.route('/backup', methods=["GET", "POST"])
 def backup():
-    return render_template('backup.html')
+    if request.method == 'POST':
+        file = request.files['backup']
+        file_path = FileSystem.save(file, app.config['UPLOAD_DIR'])
+        Zip.unzip(file_path, FileSystem.DATA_DIR)
+        FileSystem.delete_content(FileSystem.UPLOAD_DIR)
+
+        return render_template("backup.html", msg="Backup successfully restored.")
+    return render_template("backup.html", msg="")
 
 
 # https://code-maven.com/flask-upload-multiple-files
