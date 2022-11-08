c80a4790d9c24a24e8bf396b648005fe6c39b50d
Danny Kiefner <dannyk@spotify.com>
2022-11-08 20:31:56 +0100

    fix temperature funcs

--- app/lib/system_info.py
+++ app/lib/system_info.py
@@ -7,8 +7,8 @@ class SystemInfo:
 
     @staticmethod
     def gpu_temp():
-        Shell.execute("vcgencmd measure_temp | cut -d\"=\" -f 2 | cut -d\"'\" -f 1")
+        return Shell.execute("vcgencmd measure_temp | cut -d\"=\" -f 2 | cut -d\"'\" -f 1")
 
     @staticmethod
     def cpu_temp():
-        Shell.execute("cpu=$(</sys/class/thermal/thermal_zone0/temp);echo \"$((cpu/1000))\"")
+        return Shell.execute("cpu=$(</sys/class/thermal/thermal_zone0/temp);echo \"$((cpu/1000))\"")
