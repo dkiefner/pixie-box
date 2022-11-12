fe8fd273e94a72c74d607b30c5625a0842000478
Danny Kiefner <dannyk@spotify.com>
2022-11-12 22:15:03 +0100

    implement sleep timer service

--- app/service/sleep_timer.py
+++ app/service/sleep_timer.py
@@ -6,7 +6,7 @@ from lib.player import LocalFilePlayer
 from lib.shutdown import Shutdown
 from lib.store import ServiceStateStore
 
-KEY_SLEEP_TIMER_TIME_IN_SECONDS = "sleep_timer_time_in_seconds"
+KEY_SLEEP_TIMER_TIMEOUT = "sleep_timer_timeout"
 KEY_PLAYER_STOPPED_TIMESTAMP = "player_stopped_timestamp"
 
 serviceStateStore = ServiceStateStore()
@@ -14,7 +14,7 @@ player = LocalFilePlayer(serviceStateStore)
 
 
 def is_enabled():
-    return serviceStateStore.get_string(KEY_SLEEP_TIMER_TIME_IN_SECONDS) is not None
+    return serviceStateStore.get_string(KEY_SLEEP_TIMER_TIMEOUT) is not None
 
 
 def reset_timer():
@@ -27,8 +27,10 @@ def current_timestamp():
 
 
 def is_sleep_timeout_reached():
-    return current_timestamp() - serviceStateStore.get_int(KEY_PLAYER_STOPPED_TIMESTAMP) >= serviceStateStore.get_int(
-        KEY_SLEEP_TIMER_TIME_IN_SECONDS)
+    player_stopped_timestamp = serviceStateStore.get_int(KEY_PLAYER_STOPPED_TIMESTAMP)
+    sleep_timer_timeout = serviceStateStore.get_int(KEY_SLEEP_TIMER_TIMEOUT)
+
+    return current_timestamp() - player_stopped_timestamp >= sleep_timer_timeout
 
 
 def start_timer():
--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Main service to play audio ##
 # create a link for the service file to make the PixieBox reader run using systemd
 ln -s /home/pi/pixiebox/app/service/pixiebox.service /etc/systemd/system/pixiebox.service
 
@@ -7,3 +8,13 @@ ln -s /home/pi/pixiebox/app/service/pixiebox.service /etc/systemd/system/pixiebo
 systemctl daemon-reload
 systemctl enable pixiebox.service
 systemctl start pixiebox.service
+
+
+## Sleep timer service ##
+# create a link for the service file to make the sleep timer run using systemd
+ln -s /home/pi/pixiebox/app/service/sleep_timer.service /etc/systemd/system/sleep_timer.service
+
+# enable and start the sleep timer service
+systemctl daemon-reload
+systemctl enable sleep_timer.service
+systemctl start sleep_timer.service
